d7e4161ac8d21331fc663ecc204dfc796dd42f4d
Gabriella Speariett <gabriella@suade.org>
2026-04-30 08:43:20 +0100

    feat(rdb): ✨ enable arrow keys to work with rdb

--- configuration/fish/functions/rdb.fish
+++ configuration/fish/functions/rdb.fish
@@ -2,5 +2,5 @@ function rdb --description 'Open a telnet debug session in a running Docker cont
     set container $argv[1]
     set port (if test (count $argv) -gt 1; echo $argv[2]; else; echo 6899; end)
     docker exec -itu root $container \
-        bash -c "apt update -qq && apt install -qq --assume-yes --no-install-recommends telnet && telnet localhost $port"
+        bash -c "apt update -qq && apt install -qq --assume-yes --no-install-recommends telnet rlfe && rlfe telnet localhost $port"
 end
